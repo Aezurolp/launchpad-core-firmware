910225939ed709befc0f9b9a3f8c2d01f1e2221c
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2026-01-17 13:11:36 +0100

    Remove old Pro Mk3 midi callback

--- include/flash/settings.h
+++ include/flash/settings.h
@@ -8,7 +8,7 @@ extern uint8_t settings_velocity_curve; // 0-2
 extern uint8_t settings_velocity_enabled; // 0-1
 extern uint8_t settings_aftertouch_curve; // 0-2
 extern uint8_t settings_aftertouch_mode; // 0-2
-extern uint8_t settings_palette; // 0-1
+extern uint8_t settings_palette; // 0-6
 
 extern uint8_t settings_custom_palette[3][3][128]; // RGB 0-63
 
--- src/flash/settings.c
+++ src/flash/settings.c
@@ -5,6 +5,6 @@ uint8_t __attribute__((section(".cfw_bss"))) settings_velocity_curve = 0; // 0-2
 uint8_t __attribute__((section(".cfw_bss"))) settings_velocity_enabled = 0; // 0-1
 uint8_t __attribute__((section(".cfw_bss"))) settings_aftertouch_curve = 1; // 0-2
 uint8_t __attribute__((section(".cfw_bss"))) settings_aftertouch_mode = 0; // 0-2
-uint8_t __attribute__((section(".cfw_bss"))) settings_palette = 0; // 0-5
+uint8_t __attribute__((section(".cfw_bss"))) settings_palette = 0; // 0-6
 
-uint8_t __attribute__((section(".cfw_bss"))) settings_custom_palette[3][3][128];
+uint8_t __attribute__((section(".cfw_bss"))) settings_custom_palette[3][3][128] = {0};
